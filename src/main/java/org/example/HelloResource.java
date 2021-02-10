b10e45a20b1ac2cdea0a511fc79e1d3271ad7de4
nkoike <nkoike@redhat.com>
2021-02-10 23:44:06 +0900

    revert pom

--- src/main/java/org/example/HelloResource.java
+++ src/main/java/org/example/HelloResource.java
@@ -11,7 +11,7 @@ public class HelloResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        System.out.println("こんにちは");
+        // System.out.println("こんにちは");
         return "hello";
     }
 }
